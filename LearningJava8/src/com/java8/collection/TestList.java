1b0392859c3572ae48181f9f5921614091d03006
Vijay <vijay.kumar@letmecall.com>
2018-03-22 00:14:49 -0700

    Updated

--- LearningJava8/src/com/java8/collection/TestList.java
+++ LearningJava8/src/com/java8/collection/TestList.java
@@ -12,12 +12,19 @@ public class TestList {
 		List<String> fruits=Arrays.asList(arrFruits);
 		
 		// Regular loop
+		System.out.println("=====>REGULAR- LOOP");
 		for (String fruit : fruits) {
 			System.out.print(fruit+" ");
 		}
-		
-		
-		
+		// Java8
+		System.out.println("\n=====>JAVA8- LOOP");
+		fruits.forEach(fruit->System.out.print(fruit+" "));
+		System.out.println("");
+		// Iterate and Convert to upper case
+		fruits.forEach(fruit->{
+			fruit=fruit.toUpperCase();
+			System.out.print(fruit+" ");
+		});
 	}
 
 }
