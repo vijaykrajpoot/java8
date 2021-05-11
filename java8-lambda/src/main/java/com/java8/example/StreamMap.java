fa233e4e45296733c282fa1cc96e2868331ea756
Vijay Kumar Rajput <vijaykrajpoot@gmail.com>
2021-05-11 00:31:32 -0700

    Java8

---
+++ java8-lambda/src/main/java/com/java8/example/StreamMap.java
@@ -0,0 +1,30 @@
+package com.java8.example;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.function.Function;
+
+public class StreamMap {
+    public static void main(String[] args) {
+        List<Integer> l1 = Arrays.asList(1, 2, 3, 4, 5, 6, 7);
+        List<Integer> l2 = Arrays.asList(8, 9, 10, 11);
+        List<Integer> l3 = Arrays.asList(12, 13);
+
+        List<List<Integer>> lists = Arrays.asList(l1, l2, l3);
+        System.out.println(lists);
+        Function<List<Integer>, Integer> fSize = List::size;
+
+
+        lists.stream().map(fSize)
+                .forEach(System.out::println);
+        System.out.println("Map Stream ..................");
+        lists.stream().map(ll->ll.stream())
+                .forEach(System.out::println);
+
+        System.out.println("FlatMap..................");
+        lists.stream().flatMap(ll->ll.stream())
+                .forEach(System.out::println);
+
+    }
+}
